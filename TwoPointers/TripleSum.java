b9c6ef5eed6d68f0da07a923959b5e782ecb1ebe
vishnukvs <vishnu.kvs202@gmail.com>
2023-07-19 14:24:20 -0700

    Triple Sum

---
+++ TwoPointers/TripleSum.java
@@ -0,0 +1,57 @@
+package TwoPointers;
+
+import java.util.*;
+
+public class TripleSum {
+    public static List<List<Integer>> tripleSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                    res.add(triplet);
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = sc.nextInt();
+            }
+            List<List<Integer>> result = tripleSum(nums);
+
+            for (List<Integer> res : result) {
+                System.out.println(res);
+            }
+        }
+    }
+}
